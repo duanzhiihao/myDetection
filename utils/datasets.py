0d832bfc516250386d7df6c50dfd3aecf03af80d
Zhihao Duan <duanzh@bu.edu>
2020-05-15 17:57:24 -0400

    rearrange dataset and evaluation code

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,10 +1,7 @@
 import os
-import sys
-from io import StringIO
 import json
 import random
-import numpy as np
-from collections import defaultdict, OrderedDict
+from collections import defaultdict
 from pycocotools import cocoeval
 import torch
 import torchvision.transforms.functional as tvf
@@ -12,6 +9,7 @@ import torchvision.transforms.functional as tvf
 import utils.image_ops as imgUtils
 import utils.augmentation as augUtils
 from utils.structures import ImageObjects
+from evaluation.coco import coco_evaluate_json
 
 
 def get_trainingset(cfg: dict):
@@ -75,7 +73,7 @@ def get_valset(valset_name):
         gt_json = json.load(open(val_json_path, 'r'))
         eval_info = [(os.path.join(img_dir, imi['file_name']), imi['id']) \
                      for imi in gt_json['images']]
-        from .cepdof_api import evaluate_json
+        from .evaluation.cepdof import evaluate_json
         validation_func = lambda x: evaluate_json(x, val_json_path)
     elif valset_name in {'Lunch1', 'Lunch2', 'Lunch3', 'Edge_cases',
                         'High_activity', 'All_off', 'IRfilter', 'IRill',
@@ -86,7 +84,7 @@ def get_valset(valset_name):
         gt_json = json.load(open(val_json_path, 'r'))
         eval_info = [(os.path.join(img_dir, imi['file_name']), imi['id']) \
                      for imi in gt_json['images']]
-        from .cepdof_api import evaluate_json
+        from .evaluation.cepdof import evaluate_json
         validation_func = lambda x: evaluate_json(x, val_json_path)
     elif valset_name == 'debug3':
         img_dir = './images/debug3/'
@@ -115,7 +113,7 @@ def get_valset(valset_name):
         gt_json = json.load(open(val_json_path, 'r'))
         eval_info = [(os.path.join(img_dir, imi['file_name']), imi['id']) \
                      for imi in gt_json['images']]
-        from .cepdof_api import evaluate_json
+        from .evaluation.cepdof import evaluate_json
         validation_func = lambda x: evaluate_json(x, val_json_path)
     else:
         raise NotImplementedError()
@@ -316,175 +314,3 @@ class Dataset4ObjDet(torch.utils.data.Dataset):
 
 def uniform(a, b):
     return a + torch.rand(1).item() * (b-a)
-
-
-def coco_evaluate_json(dts_json, gt_json_path):
-    # json.dump(dts_json, open('./tmp.json','w'), indent=1)
-    print('Initialing validation set...')
-    # cocoGt = COCO(gt_json_path)
-    # cocoDt = cocoGt.loadRes('./tmp.json')
-    cocoEval = myCOCOeval(gt_json_path, dts_json, 'bbox')
-    cocoEval.evaluate()
-    cocoEval.accumulate()
-    # have to manually get the evaluation string 
-    # stdout = sys.stdout
-    # s = StringIO()
-    # sys.stdout = s
-    cocoEval.summarize()
-    # sys.stdout = stdout
-    # s.seek(0)
-    # s = s.read()
-    # print(s)
-    ap, ap50, ap75 = cocoEval.stats[0], cocoEval.stats[1], cocoEval.stats[2]
-    return cocoEval.summary, ap, ap50, ap75
-
-
-class myCOCOeval(cocoeval.COCOeval):
-    '''
-    Make COCOeval more flexible
-    '''
-    def __init__(self, gt_json, dt_json, iouType='segm'):
-        if not iouType:
-            print('iouType not specified. use default iouType segm')
-        self.gt_json = json.load(open(gt_json, 'r')) if isinstance(gt_json, str) \
-                       else gt_json
-        self.dt_json = json.load(open(dt_json, 'r')) if isinstance(dt_json, str) \
-                       else dt_json
-        self._preprocess()
-        self.params   = {}                  # evaluation parameters
-        self.evalImgs = defaultdict(list)
-        # per-image per-category evaluation results [KxAxI] elements
-        self.eval     = {}                  # accumulated evaluation results
-        self._gts = defaultdict(list)       # gt for evaluation
-        self._dts = defaultdict(list)       # dt for evaluation
-        self.params = cocoeval.Params(iouType=iouType) # parameters
-        self._paramsEval = {}               # parameters for evaluation
-        self.stats = []                     # result summarization
-        self.ious = {}                      # ious between all gts and dts
-        
-        self.params.imgIds = sorted([im['id'] for im in self.gt_json['images']])
-        self.params.catIds = sorted([c['id'] for c in self.gt_json['categories']])
-
-    def _preprocess(self):
-        for i, dt in enumerate(self.dt_json):
-            dt['id'] = dt.get('id', i+1)
-            dt['area'] = dt.get('area', dt['bbox'][2]*dt['bbox'][3])
-        
-    def _prepare(self):
-        '''
-        Prepare ._gts and ._dts for evaluation based on params
-        :return: None
-        '''
-        def _toMask(anns, coco):
-            # modify ann['segmentation'] by reference
-            for ann in anns:
-                rle = coco.annToRLE(ann)
-                ann['segmentation'] = rle
-        p = self.params
-        if p.useCats:
-            imgids = set(p.imgIds)
-            catids = set(p.catIds)
-            gts = [gt for gt in self.gt_json['annotations'] if \
-                   (gt['image_id'] in imgids and gt['category_id'] in catids)]
-            dts = [dt for dt in self.dt_json if \
-                   (dt['image_id'] in imgids and dt['category_id'] in catids)]
-        else:
-            raise NotImplementedError()
-            # gts=self.cocoGt.loadAnns(self.cocoGt.getAnnIds(imgIds=p.imgIds))
-            # dts=self.cocoDt.loadAnns(self.cocoDt.getAnnIds(imgIds=p.imgIds))
-
-        # convert ground truth to mask if iouType == 'segm'
-        if p.iouType == 'segm':
-            raise NotImplementedError()
-            # _toMask(gts, self.cocoGt)
-            # _toMask(dts, self.cocoDt)
-        # set ignore flag
-        for gt in gts:
-            gt['ignore'] = gt['ignore'] if 'ignore' in gt else 0
-            gt['ignore'] = 'iscrowd' in gt and gt['iscrowd']
-            if p.iouType == 'keypoints':
-                gt['ignore'] = (gt['num_keypoints'] == 0) or gt['ignore']
-        self._gts = defaultdict(list)       # gt for evaluation
-        self._dts = defaultdict(list)       # dt for evaluation
-        for gt in gts:
-            self._gts[gt['image_id'], gt['category_id']].append(gt)
-        for dt in dts:
-            self._dts[dt['image_id'], dt['category_id']].append(dt)
-        self.evalImgs = defaultdict(list)   # per-image per-category evaluation results
-        self.eval     = {}                  # accumulated evaluation results
-    
-    def summarize(self):
-        '''
-        Compute and display summary metrics for evaluation results.
-        Note this functin can *only* be applied on the default parameter setting
-        '''
-        self.summary = ''
-        def _summarize( ap=1, iouThr=None, areaRng='all', maxDets=100 ):
-            p = self.params
-            iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
-            titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
-            typeStr = '(AP)' if ap==1 else '(AR)'
-            iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
-                if iouThr is None else '{:0.2f}'.format(iouThr)
-
-            aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
-            mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
-            if ap == 1:
-                # dimension of precision: [TxRxKxAxM]
-                s = self.eval['precision']
-                # IoU
-                if iouThr is not None:
-                    t = np.where(iouThr == p.iouThrs)[0]
-                    s = s[t]
-                s = s[:,:,:,aind,mind]
-            else:
-                # dimension of recall: [TxKxAxM]
-                s = self.eval['recall']
-                if iouThr is not None:
-                    t = np.where(iouThr == p.iouThrs)[0]
-                    s = s[t]
-                s = s[:,:,aind,mind]
-            if len(s[s>-1])==0:
-                mean_s = -1
-            else:
-                mean_s = np.mean(s[s>-1])
-            summ_str = iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s)
-            # print(summ_str)
-            self.summary += summ_str + '\n'
-            return mean_s
-        def _summarizeDets():
-            stats = np.zeros((12,))
-            stats[0] = _summarize(1)
-            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
-            stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
-            stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
-            stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
-            stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
-            return stats
-        def _summarizeKps():
-            stats = np.zeros((10,))
-            stats[0] = _summarize(1, maxDets=20)
-            stats[1] = _summarize(1, maxDets=20, iouThr=.5)
-            stats[2] = _summarize(1, maxDets=20, iouThr=.75)
-            stats[3] = _summarize(1, maxDets=20, areaRng='medium')
-            stats[4] = _summarize(1, maxDets=20, areaRng='large')
-            stats[5] = _summarize(0, maxDets=20)
-            stats[6] = _summarize(0, maxDets=20, iouThr=.5)
-            stats[7] = _summarize(0, maxDets=20, iouThr=.75)
-            stats[8] = _summarize(0, maxDets=20, areaRng='medium')
-            stats[9] = _summarize(0, maxDets=20, areaRng='large')
-            return stats
-        if not self.eval:
-            raise Exception('Please run accumulate() first')
-        iouType = self.params.iouType
-        if iouType == 'segm' or iouType == 'bbox':
-            summarize = _summarizeDets
-        elif iouType == 'keypoints':
-            summarize = _summarizeKps
-        self.stats = summarize()
